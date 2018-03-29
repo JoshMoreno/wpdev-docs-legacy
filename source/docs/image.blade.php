99b5a8612e8b3790ccbe2b6ccf02a51a434087f4
Josh Moreno <Josh@GaslampMedia.com>
2018-03-29 07:09:03 -0700

    fix code formatting

--- source/docs/image.blade.php
+++ source/docs/image.blade.php
@@ -17,14 +17,14 @@
 
             echo Image::create(80, 'large');
             /* Outputs something like...
-            <img src="https://site.com/wp-content/uploads/2018/02/sample-image-260x300.jpg"
+            &lt;img src="https://site.com/wp-content/uploads/2018/02/sample-image-260x300.jpg"
                  width="260"
                  height="300"
                  alt="This is the alt text"
                  srcset="https://site.com/wp-content/uploads/2018/02/sample-image-260x300.jpg 260w, https://site.com/wp-content/uploads/2018/02/sample-image-768x887.jpg 768w, https://site.com/wp-content/uploads/2018/02/sample-image-887x1024.jpg 887w, https://site.com/wp-content/uploads/2018/02/sample-image.jpg 1143w"
                  sizes="(max-width: 260px) 100vw, 260px"
                  title="This is the title"
-            >
+            &gt;
             */
 
             echo Image::create(80, 'large')
@@ -32,21 +32,22 @@
             ->setAttribute('data-cool', 'pretty much')
             ->setAttribute('data-no-val')
             ->withCaption();
-            /* Only if there actually is a caption it outputs something like... */
-            //<figure>
-            //    <img src="https://site.com/wp-content/uploads/2018/02/sample-image-260x300.jpg"
-            //         width="260"
-            //         height="300"
-            //         alt="This is the alt text"
-            //         srcset="https://site.com/wp-content/uploads/2018/02/sample-image-260x300.jpg 260w, https://site.com/wp-content/uploads/2018/02/sample-image-768x887.jpg 768w, https://site.com/wp-content/uploads/2018/02/sample-image-887x1024.jpg 887w, https://site.com/wp-content/uploads/2018/02/sample-image.jpg 1143w"
-            //         sizes="(max-width: 260px) 100vw, 260px"
-            //         title="This is the title"
-            //         class="img-responsive someclass"
-            //         data-cool="pretty much"
-            //         data-no-val
-            //    >
-            //    <figcaption>This is the caption</figcaption>
-            //</figure>
+            /* Only if there actually is a caption it outputs something like...
+            &lt;figure&gt;
+            &lt;img src="https://site.com/wp-content/uploads/2018/02/sample-image-260x300.jpg"
+                     width="260"
+                     height="300"
+                     alt="This is the alt text"
+                     srcset="https://site.com/wp-content/uploads/2018/02/sample-image-260x300.jpg 260w, https://site.com/wp-content/uploads/2018/02/sample-image-768x887.jpg 768w, https://site.com/wp-content/uploads/2018/02/sample-image-887x1024.jpg 887w, https://site.com/wp-content/uploads/2018/02/sample-image.jpg 1143w"
+                     sizes="(max-width: 260px) 100vw, 260px"
+                     title="This is the title"
+                     class="img-responsive someclass"
+                     data-cool="pretty much"
+                     data-no-val
+                &gt;
+            &lt;figcaption&gt;This is the caption&lt;/figcaption&gt;
+            &lt;/figure&gt;
+            */
 
             // You don't have to use it to output an image tag.
             $image = Image::create(80, 'large');
