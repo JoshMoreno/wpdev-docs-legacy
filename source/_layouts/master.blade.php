84a5c07cc6b98e45ac5714b10f4a0b11c8861de6
Josh Moreno <josh@joshmoreno.com>
2018-03-29 20:46:16 -0700

    add mix versioning

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <link rel="stylesheet" href="/assets/css/main.css">
+    <link rel="stylesheet" href="{{mix('/css/main.css')}}">
     <!--use just the brands and solid styles-->
     <script src="https://use.fontawesome.com/releases/v5.0.8/js/brands.js"></script>
     <script src="https://use.fontawesome.com/releases/v5.0.8/js/solid.js"></script>
@@ -30,6 +30,6 @@
         <div class="fixed-mobile-nav__circle-overlay"></div>
         @include('_partials.menuIcon')
     </div>
-    <script src="/assets/js/main.js"></script>
+    <script src="{{mix('/js/main.js')}}"></script>
 </body>
 </html>
