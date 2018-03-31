2cfdb9923a0c75e740537de995e7920ef7a994be
Josh Moreno <josh@joshmoreno.com>
2018-03-31 09:12:45 -0700

    hash versioning instead of query string

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,12 @@
 const mix = require('laravel-mix');
 const jigsaw = require('@joshmoreno/jigsaw');
 
+const fs = require('file-system');
+const _ = require('lodash');
+const jsonfile = require('jsonfile');
+
+const mixManifest = 'source/assets/mix-manifest.json';
+
 mix.disableSuccessNotifications();
 
 mix.setPublicPath('source/assets/');
@@ -11,6 +17,28 @@ mix.webpackConfig({
 
 mix.js('source/_assets/js/main.js', 'js/')
     .sass('source/_assets/sass/main.scss', 'css/')
-    .version();
+    .version()
+	.then(function () {
+		jsonfile.readFile(mixManifest, function (err, obj) {
+			const newJson = {};
+			_.forIn(obj, function (value, key) {
+				const newFilename = value.replace(/([^\.]+)\.([^\?]+)\?id=(.+)$/g, '$1.$3.$2');
+				const oldAsGlob = value.replace(/([^\.]+)\.([^\?]+)\?id=(.+)$/g, '$1.$2');
+				// copy as new versioned
+				fs.copyFile('source/assets' + key, 'source/assets' + newFilename, function(err) {
+					if (err) console.error(err);
+				});
+
+				// delete old versioned file
+				fs.unlink('./source/assets' + oldAsGlob, (err) => {
+					if (err) throw err;
+				});
+				newJson[key] = newFilename;
+			});
+			jsonfile.writeFile(mixManifest, newJson, {spaces: 2}, function (err) {
+				if (err) console.error(err);
+			});
+		});
+	});
 
 jigsaw.watch();
